b57fd85ee54b7b7c67ad8055a4a5a8808c4b3df8
klich404 <klich4010@gmail.com>
2021-04-14 22:48:51 -0500

    comment add

--- empty_cmd_check.c
+++ empty_cmd_check.c
@@ -4,6 +4,7 @@
  * empty_cmd_check - checkes wether a command line has characters
  * or is only empty spaces
  * @n_string: a string that contains all the arguments in the command line
+ * Return: 0 or 1
   */
 
 int empty_cmd_check(char *n_string)
