3e3dd1ebb3bf53acaa026322b96111d39b0f57c9
Julianeme <juliantve@gmail.com>
2021-04-14 23:49:12 -0500

    code resotred to a previous point

--- simple_shell.c
+++ simple_shell.c
@@ -1,28 +1,12 @@
 #include "holberton.h"
 
 /**
- * handle_sigtstp - cancels the ctrl + c signal, allowing the program
- * to run even ctrl + c i called
- * @sig: the signal
- */
-void handle_sigtstp(int sig)
-{
-	_printf("stop not allowed\n")
-}
-
-/**
  * main - the main function
  * Return: 0
  */
 
 int main(void)
 {
-	struct sigaction sa;
-
-	sa.sa_handler = &handle_sigtstp;
-	sigaction(SIGTSTP, &sa, NULL);
-	sa.sa_flags - SA_RESTART;
-
 	if (isatty(0) == 0)
 	{
 		non_interactive_mode();
